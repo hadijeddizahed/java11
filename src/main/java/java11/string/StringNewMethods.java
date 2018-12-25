e46c9b652f6f84aad86e02b54e0962f6d851272e
SSH <hadijeddizahed@gmail.com>
2018-12-25 13:49:28 +0330

    string new methods!

--- src/main/java/java11/string/StringNewMethods.java
+++ src/main/java/java11/string/StringNewMethods.java
@@ -4,8 +4,29 @@ public class StringNewMethods {
     public static void main(String[] args) {
         var hiJava = "Hi Java 11";
 
-        var newContent = hiJava.repeat(2);// Hi Java 11Hi Java 11
+
+        var newContent = hiJava.repeat(2);// the repeat() method repeats string n times.
         System.out.println(newContent.equals(hiJava.concat(hiJava)));
+
+        var isBlank = " ";
+        System.out.println(isBlank.isBlank());
+
+        var isEmpty = "";
+        System.out.println(isEmpty.isEmpty());
+
+        var text = " Hello World! ";
+        System.out.println(text);
+        var textStrip = text.strip(); // " Hello World! "--> "Hello World!"
+        System.out.println(textStrip);
+
+        var stripTrailing = text.stripTrailing().concat("again!");
+        System.out.println(stripTrailing);
+
+        var stripLeading = text.stripLeading().concat("again!");
+        System.out.println(stripLeading);
+
+        var lines = "this\ntext\nhas\n?\nlines!";
+        System.out.println(lines.replace('\n',' ')+lines.lines().count());
 
     }
 

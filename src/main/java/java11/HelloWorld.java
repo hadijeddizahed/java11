df1a1b9bd42d7d27f3d5a4cba697b244a6b44e82
SSH <hadijeddizahed@gmail.com>
2018-12-25 12:52:26 +0330

    Welcome var type!

---
+++ src/main/java/java11/HelloWorld.java
@@ -0,0 +1,9 @@
+package java11;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        var var = "Hello World! in Java 11";
+        System.out.println(var);
+    }
+}

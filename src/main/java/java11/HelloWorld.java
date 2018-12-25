1f7abb13192c101102cd1da4dc458c0a629c3408
SSH <hadijeddizahed@gmail.com>
2018-12-25 12:55:37 +0330

    Update HelloWorld.java

--- src/main/java/java11/HelloWorld.java
+++ src/main/java/java11/HelloWorld.java
@@ -3,7 +3,7 @@ package java11;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        var var = "Hello World! in Java 11";
+        var var = "Hello World! in Java 11"; // I thought it isn't possible define variable like this. while this impossible. int int = 2;
         System.out.println(var);
     }
 }
